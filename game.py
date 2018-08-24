830d23183e5b04389ab0a4b9609107ba83b0904a
sweta jena <swetajena989@gmail.com>
2018-08-24 18:30:13 +0530

    add secret code

--- game.py
+++ game.py
@@ -4,15 +4,15 @@ newMessage = ''
 secret code = 2048
 code = input('enter the code')
 if secret_code==code:
-  message = input('please enter ur message:')
-  for character in message:
-   if character in alphabet:
-     position = alphabet.find(character)
-     newPosition = (position+key)%26
+   message = input('please enter ur message:')
+   for character in message:
+    if character in alphabet:
+      position = alphabet.find(character)
+      newPosition = (position+key)%26
       newCharacter = alphabet[newPosition]
       newMessage+=newCharacter
-   else:
-     newMessage+=character
-  print('your new message is:',newMessage)
+    else:
+      newMessage+=character
+   print('your new message is:',newMessage)
 else:
-  print('you are not allowed to message')
+   print('you are not allowed to message')
