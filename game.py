0a3b6ed5807503bb5ebd98b7bdad8cefa54a686b
sweta jena <swetajena989@gmail.com>
2018-08-24 07:55:55 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,14 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = 5
+newMessage = ''
+message = input('please enter ur message:')
+for character in message:
+  if character in alphabet:
+    position = alphabet.find(character)
+    newPosition = (position+key)%26
+    newCharacter = alphabet[newPosition]
+    newMessage+=newCharacter
+  else:
+    newMessage+=character
+print('your new message is:',newMessage)
+    
