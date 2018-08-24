5ee24c2b59b6cae49c82b433e9de79a3ceadc6c6
sweta jena <swetajena989@gmail.com>
2018-08-24 18:29:01 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,14 +1,18 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 key = 5
 newMessage = ''
-message = input('please enter ur message:')
-for character in message:
-  if character in alphabet:
-    position = alphabet.find(character)
-    newPosition = (position+key)%26
-    newCharacter = alphabet[newPosition]
-    newMessage+=newCharacter
-  else:
-    newMessage+=character
-print('your new message is:',newMessage)
-    
+secret code = 2048
+code = input('enter the code')
+if secret_code==code:
+  message = input('please enter ur message:')
+  for character in message:
+   if character in alphabet:
+     position = alphabet.find(character)
+     newPosition = (position+key)%26
+      newCharacter = alphabet[newPosition]
+      newMessage+=newCharacter
+   else:
+     newMessage+=character
+  print('your new message is:',newMessage)
+else:
+  print('you are not allowed to message')
